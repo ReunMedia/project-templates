4292516ad40da4016e5eb84de07eb2a7b084834c
Kimmo Salmela <kimmo.salmela@reun.eu>
2024-06-13 16:16:58 +0300

    Update PHP CS Ficer configuration

--- base/webdev/php/.php-cs-fixer.php
+++ base/webdev/php/.php-cs-fixer.php
@@ -4,32 +4,41 @@
  * Reun Media PHP CS Fixer configuration file.
  *
  * @author Reun Media <company@reun.eu>
- * @copyright 2020-2022 Reun Media
+ * @copyright 2020-2024 Reun Media
  *
  * @see https://github.com/Reun-Media/project-templates/blob/master/base/webdev/php/.php-cs-fixer.php
  *
- * @version 2.0.0
+ * @version 3.0.0
  *
- * @since 2022-12-28 Last modification date
+ * @since 2024-06-13 Last modification date
  */
 
 declare(strict_types=1);
 
-$config = new PhpCsFixer\Config();
+use PhpCsFixer\Config;
+
+$config = new Config();
 
 return $config
-    ->setRiskyAllowed(true)
     ->setRules([
-        '@PSR12' => true,
-        '@PSR12:risky' => true,
-        '@Symfony' => true,
-        '@Symfony:risky' => true,
-        'align_multiline_comment' => true,
-        'array_indentation' => true,
-        'declare_strict_types' => true,
-        'multiline_whitespace_before_semicolons' => true,
-        'operator_linebreak' => true,
-        'php_unit_internal_class' => true,
-        'php_unit_test_class_requires_covers' => true,
-        'single_quote' => false,
-    ]);
+        "@PER-CS2.0" => true,
+        "@PhpCsFixer" => true,
+
+        "declare_strict_types" => true,
+
+        // Prefer double quotes
+        "single_quote" => false,
+
+        // Allows us to use /** @disregard */
+        "phpdoc_to_comment" => false,
+
+        // These two rules allow us to use `#region` comments for region
+        // folding
+        "single_line_comment_spacing" => false,
+        "single_line_comment_style" => [
+            "comment_types" => [
+                'asterisk',
+            ],
+        ],
+    ])
+;
