3b8991071fb1b2da00ac9a3fe78378b3c378b4cf
Kimmo Salmela <kimmo.salmela@reun.eu>
2024-09-13 15:47:51 +0300

    Add Prettier config

---
+++ base/webdev/prettier.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * Reun Media Prettier configuration.
+ *
+ * @author Reun Media <company@reun.eu>
+ * @copyright 2020 Reun Media
+ *
+ * @see https://github.com/Reun-Media/project-templates/blob/master/base/webdev/prettier.config.mjs
+ *
+ * @version 3.0.0
+ */
+
+module.exports = {
+  plugins: [
+    // "./node_modules/prettier-plugin-twig-melody", // Enable if using Twig
+    "prettier-plugin-packagejson",
+  ],
+};
