fde3e238315aede9713a9f73211e4d634704660a
Kimmo Salmela <kimmo.salmela@reun.eu>
2024-11-14 20:04:43 +0200

    Update and fix ESLint configuration

--- base/webdev/frontend/eslint.config.mjs
+++ base/webdev/frontend/eslint.config.mjs
@@ -6,25 +6,28 @@
  * @author Reun Media <company@reun.eu>
  * @copyright 2024 Reun Media
  *
- * @see https://github.com/Reun-Media/project-templates/blob/master/base/webdev/php/.php-cs-fixer.php
+ * @see https://github.com/Reun-Media/project-templates/blob/main/base/webdev/frontend/eslint.config.mjs
  *
- * @version 1.0.0
+ * @version 1.1.0
  */
 
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+  // @ts-expect-error configuration contains deprecated rules
   eslintConfigPrettier,
   {
     rules: {
       "no-console": ["warn", { allow: ["warn", "error"] }],
       "sort-imports": "warn",
     },
-    ignores: ["**/dist/"],
+  },
+  {
+    ignores: ["**/dist/*", "vendor/*"],
   }
 );
